a65d5c595cb336fc3f4ae1fb7e45a105dfb1e4df
Tim <harvard90873@gmail.com>
2022-03-12 11:27:30 +0100

    dark theme dragontail

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import { DragontailProvider } from "dragontail-experimental";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <DragontailProvider theme="dark">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </DragontailProvider>
   );
 };
 
